ffd4848594df34787f299ebd70cd5876fe29c2b8
nkovalec <nikitakovalec@gmail.com>
2022-06-17 14:07:50 +0300

    add del functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+
 let id = 0
 
 function App() {
@@ -6,74 +7,77 @@ function App() {
   let [arr, setArr] = useState([]);
   const disabled = !value;
 
-  function save () {
+  function save() {
     id += 1;
     setArr([...arr, {value, id}]);
     setValue("")
   }
 
-  function del (id) {
-      setArr([...arr.slice(id)])
+  function del(id) {
+    setArr(arr.filter(obj => obj.id !== id))
+    // let findId = arr.findIndex(obj => obj.id === id);
+    // setArr(arr.splice(1, findId))
   }
 
   return <>
     <input
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        style={{
-            width: 200,
-            height: 35,
-            margin: 15,
-            padding: "5px 10px",
-            border: "1px solid #bdbdbd",
-            borderRadius: 4
-        }}
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+      style={{
+        width: 200,
+        height: 35,
+        margin: 15,
+        padding: "5px 10px",
+        border: "1px solid #bdbdbd",
+        borderRadius: 4
+      }}
     />
     <button
-        disabled={disabled}
-        onClick={save}
+      disabled={disabled}
+      onClick={save}
+      style={{
+        border: "none",
+        borderRadius: 5,
+        background: disabled ? "#333333" : "#4676D7",
+        color: "#fff",
+        padding: "15px 20px",
+        fontSize: 16
+      }}
+    >Добавить
+    </button>
+    {arr.map(function ({value, id} ) {
+      return <div
+        key={id}
         style={{
+          display: "flex"
+        }}>
+        <div
+          style={{
+            width: 200,
+            height: 40,
+            margin: 15,
+            padding: "5px 10px",
+            border: "1px solid #4676D7",
+            borderRadius: 4
+          }}
+        >{value}</div>
+        <button
+          onClick={() => del(id)}
+          style={{
+            width: 111,
+            height: 52,
             border: "none",
-            borderRadius: 5,
-            background: disabled ? "#333333" : "#4676D7",
-            color: "#fff",
+            marginTop: 15,
             padding: "15px 20px",
+            borderRadius: 4,
+            background: "#4676D7",
+            color: "#fff",
             fontSize: 16
-        }}
-    >Добавить</button>
-    {arr.map(function ({value, id}) {
-        return <div
-            key={id}
-            style={{
-                display: "flex"
-            }}>
-            <div
-                key={id + 1}
-                style={{
-                    width: 200,
-                    height: 40,
-                    margin: 15,
-                    padding: "5px 10px",
-                    border: "1px solid #4676D7",
-                    borderRadius: 4
-                }}
-            >{value}</div>
-            <button
-                key={id + 2}
-                onClick={() => del(id)}
-                style={{
-                    width: 111,
-                    height: 52,
-                    border: "none",
-                    marginTop: 15,
-                    padding: "15px 20px",
-                    borderRadius: 4,
-                    background: "#4676D7",
-                    color: "#fff",
-                    fontSize: 16
-                }}
-            >Удалить</button>
-        </div>})}
+          }}
+        >Удалить
+        </button>
+      </div>
+    })}
   </>
 }
 
