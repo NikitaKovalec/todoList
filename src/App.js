6a71ec98b3d7b0fc17cb59ab1b0dac7507de4337
nkovalec <nikitakovalec@gmail.com>
2022-06-20 20:57:03 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,113 +1,22 @@
 import React, {useState} from 'react';
 import Editing from './Components/Editing';
-
-let id = 0
+import Form from './Components/Form';
+let id = 0;
 
 function App() {
-  let [value, setValue] = useState("")
   let [arr, setArr] = useState([])
-  const disabled = !value
-
-  function save() {
-    id += 1
-    setArr([...arr, {value, id}])
-    setValue("")
-  }
+  let [value, setValue] = useState("")
 
   function del(id) {
     setArr(arr.filter(obj => obj.id !== id))
   }
 
   return <>
-    <input
-      value={value}
-      onChange={(event) => setValue(event.target.value)}
-      style={{
-        width: 200,
-        height: 35,
-        margin: 15,
-        padding: "5px 10px",
-        border: "1px solid #bdbdbd",
-        borderRadius: 4
-      }}
-    />
-    <button
-      disabled={disabled}
-      onClick={save}
-      style={{
-        border: "none",
-        borderRadius: 5,
-        background: disabled ? "#333333" : "#4676D7",
-        color: "#fff",
-        padding: "15px 20px",
-        fontSize: 16
-      }}
-    >
-      Добавить
-    </button>
-    <Editing value={value} />
-    {arr.map(({value, id}) =>
-      <div
-        key={id}
-        style={{
-          display: "flex",
-        }}
-      >
-        <div
-          style={{
-            marginTop: 15
-          }}
-        >
-          {"Задача №" + id}
-        </div>
-        <div
-          style={{
-            width: 200,
-            height: 40,
-            margin: 15,
-            padding: "5px 10px",
-            border: "1px solid #4676D7",
-            borderRadius: 4,
-            overflow: "hidden",
-            textOverflow: "ellipsis"
-          }}
-        >
-          {value}
-        </div>
-        <button
-          style={{
-            width: 150,
-            height: 52,
-            border: "none",
-            margin: "15px 5px 0px 0px",
-            padding: "15px 20px",
-            borderRadius: 4,
-            background: "#4676D7",
-            color: "#fff",
-            fontSize: 16
-          }}
-        >
-          Редактировать
-        </button>
-        <button
-          onClick={() => del(id)}
-          style={{
-            width: 111,
-            height: 52,
-            border: "none",
-            marginTop: 15,
-            padding: "15px 20px",
-            borderRadius: 4,
-            background: "#d50000",
-            color: "#fff",
-            fontSize: 16
-          }}
-        >
-          Удалить
-        </button>
-      </div>
+    <Form arr={arr} setArr={setArr} value={value} setValue={setValue} id={id}/>
+    {arr.map(({value, id})=>
+      <Editing id={id} value={value}/>
     )}
-  </>
+    </>
 }
 
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,40 @@
+function Form({setArr, arr, value, setValue, id}) {
+  const disabled = !value
+
+  function save() {
+    id += 1
+    setArr([...arr, {value, id}])
+    setValue("")
+  }
+
+  return <>
+    <input
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+      style={{
+        width: 200,
+        height: 35,
+        margin: 15,
+        padding: "5px 10px",
+        border: "1px solid #bdbdbd",
+        borderRadius: 4
+      }}
+    />
+    <button
+      disabled={disabled}
+      onClick={save}
+      style={{
+        border: "none",
+        borderRadius: 5,
+        background: disabled ? "#333333" : "#4676D7",
+        color: "#fff",
+        padding: "15px 20px",
+        fontSize: 16
+      }}
+    >
+      Добавить
+    </button>
+  </>
+}
+
+export default Form
