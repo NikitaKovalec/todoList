e3811a751f2df2c6a1ec90ed9253d963dd3c0941
nkovalec <nikitakovalec@gmail.com>
2022-06-23 20:22:33 +0300

    add localStorage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Task from './Components/Task';
 import Form from './Components/Form';
 
 let id = 0
 
 function App() {
-  let [arr, setArr] = useState([])
+  let [arr, setArr] = useState(JSON.parse(localStorage.getItem('task')) || [])
   let [value, setValue] = useState("")
+
+  useEffect(() => {
+    localStorage.setItem('task', JSON.stringify(arr))
+  }, [arr])
 
   function del(id) {
     setArr(arr.filter(obj => obj.id !== id))
   }
 
   function changeValue(id, inputValue) {
-    let findValue = arr.find(obj => obj.id == id)
+    let findValue = arr.find(obj => obj.id === id)
     findValue.value = inputValue
     setArr([...arr])
   }
