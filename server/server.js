4c32926e582d122d3863a7354b9c5bb9851d6ebb
NikitaKovalec <nikitakovalec@gmail.com>
2022-06-28 19:33:22 +0300

    testing get

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3100
+let arr = []
 
 app.get('/', (req, res) => {
-  res.send('Иди нахуй! Это приватный сервер')
+  let obj = {
+    value,
+    id
+  }
+  res.send(JSON.stringify([...arr, obj]))
 })
 
 app.listen(port, () => {
