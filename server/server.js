5fc4a4b392db5d178fe333c409167439b899c325
nkovalec <nikitakovalec@gmail.com>
2022-06-29 21:08:34 +0300

    fix code

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 3100
 const tasks = []
 id = 0
-value = ''
 
 app.use(express.json())
 
@@ -13,15 +12,12 @@ app.get('/tasks', (req, res) => {
 
 app.post('/tasks', (req, res) => {
   const task = req.body
-  task.value = value
+  task.value = " Какой-то таск"
   task.id = id++
 
   tasks.push(task)
   res.status(201)
   res.send('ХУЕК')
-})
-app.put('/task/:id',(req, res) => {
-  res.send(req.params.id);
 })
 
 app.listen(port, () => {
