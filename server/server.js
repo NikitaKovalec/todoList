2dca6edd9c7f94cb7bd755f3d2ea694faf92af58
nkovalec <nikitakovalec@gmail.com>
2022-07-01 11:54:05 +0300

    fix code

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3100
 const tasks = []
-const task = {}
-const newTask = {}
 
 id = 0
 
@@ -15,28 +13,16 @@ app.get('/tasks/', (req, res) => {
 
 app.post('/tasks', (req, res) => {
   if (req.body.value) {
+    const task = {}
     task.value = req.body.value
     task.id = id++
 
     tasks.push(task)
     res.status(201)
 
-    res.send(JSON.stringify(task))
+    res.json(task)
   } else {
     res.status(400).send('Поле value обязательно')
-  }
-})
-
-app.put('/tasks/:id/', (req, res) => {
-  newTask.id = tasks.find(obj => obj.id === req.params.id);
-
-  if(newTask.id){
-    task.id = newTask.id
-    task.value = newTask.value
-
-    res.send(JSON.stringify(task))
-  } else {
-    res.status(400).send('Не найдено')
   }
 })
 
