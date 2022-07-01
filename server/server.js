ddcef6d7a2821238be4135506f564f8e94f43893
nkovalec <nikitakovalec@gmail.com>
2022-07-01 11:28:09 +0300

    add PUT method

--- server/server.js
+++ server/server.js
@@ -2,27 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3100
 const tasks = []
+const task = {}
+const newTask = {}
 
 id = 0
 
 app.use(express.json())
 
-app.get('/tasks', (req, res) => {
+app.get('/tasks/', (req, res) => {
   res.json(tasks)
+  console.log(req.params.id)
 })
 
 app.post('/tasks', (req, res) => {
-
   if (req.body.value) {
-    const task = {}
     task.value = req.body.value
     task.id = id++
+
     tasks.push(task)
     res.status(201)
-    res.send('ХУЕК')
-  }else {
+
+    res.send(JSON.stringify(task))
+  } else {
     res.status(400).send('Поле value обязательно')
   }
+})
+
+app.put('/tasks/:id/', (req, res) => {
+  req.params.id = task.id
+
+  task.id = newTask.id
+  task.value = newTask.value
+  res.send(JSON.stringify(task))
 })
 
 app.listen(port, () => {
