dcb832058ffa8b9952a506ec7abb7b0bf3a1d493
nkovalec <nikitakovalec@gmail.com>
2022-07-14 23:56:11 +0300

    fix code

--- src/App.js
+++ src/App.js
@@ -7,23 +7,38 @@ let id = 0
 function App() {
   let [arr, setArr] = useState([])
   let [value, setValue] = useState("")
+  let [isLoading, setIsLoading] = useState(true)
+  let [isError, setIsError] = useState(false)
 
-  useEffect(async () => {
+  const fetchingTasks = async () => {
     try {
-      const response = await fetch('http://localhost:3100/tasks', {
+      const response = await fetch('http://localhost:3100/tasaks', {
         mode: 'cors'
       })
       if (response.ok) {
         const data = await response.json()
-        setArr(JSON.parse(data))
+        setArr(JSON.parse(JSON.stringify(data)))
+        setIsLoading(false)
       } else {
         throw 'err'
       }
     } catch (e) {
       console.log('Ошибка загрузки тасков')
+      setIsError(true)
     }
-  }, [])
+  }
 
+  function isLoadingOrError () {
+    if (arr) {
+      return 'Загрузка данных...'
+    } else {
+      return 'Ошибка загрузки данных'
+    }
+  }
+
+  useEffect(() => {
+    fetchingTasks()
+  }, [])
 
   function del(id) {
     if (window.confirm("Удаляю?")) {
@@ -48,14 +63,16 @@ function App() {
           save={save}
           value={value}
     />
-    {arr.map(({value, id}) =>
-      <Task key={id}
-            id={id}
-            value={value}
-            changeValue={changeValue}
-            del={del}
-      />
-    )}
+    {isError ? <div style={{marginLeft: 15}}>Ошибка загрузки данных...</div> : <></>}
+    {isLoading ? <div style={{marginLeft: 15}}>Загрузка данных...</div> :
+      arr.map(({value, id}) =>
+        <Task key={id}
+              id={id}
+              value={value}
+              changeValue={changeValue}
+              del={del}
+        />
+      )}
   </>
 }
 
