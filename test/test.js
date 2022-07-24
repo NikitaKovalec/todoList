7d33723e5932b01c510b19258582a7747aa589bc
nikita <nikitakovalec@gmail.com>
2022-07-24 17:45:26 +0300

    fix code

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 const puppeteer = require('puppeteer');
 
 let scrape = async () => {
-  const browser = await puppeteer.launch({headless: false})
-  const page = await browser.newPage()
-  await page.goto('http://localhost:3000')
-  await page.click('#root > input')
-  await page.type('#root > input', Date.now().toString())
-  await page.click('#root > button')
-  await page.waitForTimeout(500)
-  // await browser.close()
+    const browser = await puppeteer.launch({headless: false})
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000')
+    await page.click('#root > input')
+    await page.type('#root > input', Date.now().toString())
+    await page.click('#root > button')
+    await page.waitForTimeout(500)
+    // await browser.close()
 }
 
 scrape()
