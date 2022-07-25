d7ad48f09f7c5cf499da7769548210fc6f3062d5
nikita <nikitakovalec@gmail.com>
2022-07-25 12:46:25 +0300

    adding e2e test app

--- test/test.js
+++ test/test.js
@@ -6,9 +6,20 @@ let scrape = async () => {
     await page.goto('http://localhost:3000')
     await page.click('#root > input')
     await page.type('#root > input', Date.now().toString())
+    const resultInput = await page.evaluate(() => {
+        return document.querySelector('#root > input').value
+    })
     await page.click('#root > button')
     await page.waitForTimeout(500)
-    // await browser.close()
+    const resultDiv = await page.evaluate(() => {
+        return document.querySelector('#root > div > div:nth-child(2)').innerText
+    })
+
+    if (resultDiv === resultInput) {
+        await browser.close()
+    } else {
+        console.log('Таск не найден')
+    }
 }
 
 scrape()
