90c54575ac2160d44f97dcea0f72e91476cca1ba
nikita <nikitakovalec@gmail.com>
2022-07-28 15:52:21 +0300

    fix code

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose()
-const fs = require('fs')
 const port = 3100
 app.use(cors())
 
@@ -11,64 +10,44 @@ const db = new sqlite3.Database('./taskList.db', sqlite3.OPEN_READWRITE, (err) =
     console.log('Успешно')
 })
 
-// db.run(`CREATE TABLE tasks(ID INTEGER PRIMARY KEY, value)`, (err) => {
-//     if (err) return console.log('Ошибка')
-//     console.log('Успешно')
-// })
-
-if (!fs.existsSync('nextTaskId.json')) {
-    fs.writeFileSync('nextTaskId.json', '1')
-}
-if (!fs.existsSync('taskList.json')) {
-    fs.writeFileSync('taskList.json', '[]')
-}
-
-let tasks = JSON.parse(fs.readFileSync('taskList.json', 'utf-8'))
-let nextTaskId = JSON.parse(fs.readFileSync('nextTaskId.json', 'utf-8'))
+db.run(`CREATE TABLE tasks (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, value CHAR)`, (err) => {
+    if (err) return console.log('Ошибка создания - таблица уже создана')
+    console.log('Успешно')
+})
 
 app.use(express.json())
 
-app.get('/tasks/', (req, res) => {
-    async function getTasks() {
-        return new Promise((resolve, reject) => {
-            db.all(`SELECT * FROM tasks`, [], (err, rows) => {
-                if (err) reject(err)
-                else {
-                    resolve(rows)
-                    res.json(tasks)
-                    console.log('Успешно')
-                }
-            })
+app.get('/tasks/', async (req, res) => {
+    const tasks = await new Promise((resolve, reject) => {
+        db.all(`SELECT * FROM tasks`, [], (err, rows) => {
+            if (err) reject(err)
+            else resolve(rows)
         })
-    }
-    getTasks().then(data=> data)
+    })
+    res.json(tasks)
 })
 
-app.post('/tasks/', (req, res) => {
+app.post('/tasks/', async (req, res) => {
     if (req.body.value) {
-        const task = {}
-        task.value = req.body.value
-        task.id = nextTaskId
-        nextTaskId++
-        let newTasks = [...tasks]
-
+        const {id, value} = req.body
         try {
-            newTasks.push(task)
-            fs.writeFileSync('taskList.json', JSON.stringify(newTasks))
-            tasks = newTasks
-            fs.writeFileSync('nextTaskId.json', JSON.stringify(nextTaskId))
-            async function postTask() {
-                return new Promise((resolve, reject) => {
-                    db.run(`INSERT INTO tasks (id, value) VALUES (?,?)`, [task.id, task.value.toString()], (err) => {
-                        if (err) reject(err)
-                        else {
-                            res.json(task)
-                            console.log('Успешно')
-                        }
-                    })
+            const taskDB = await new Promise((resolve, reject) => {
+                db.run(`INSERT INTO tasks (id, value) VALUES (?,?)`, [id, value.toString()], (err) => {
+                    if (err) reject(err)
+                    else {
+                        resolve(req.body)
+                    }
                 })
-            }
-            postTask().then(data => data)
+            })
+            const lastTask = await new Promise((resolve, reject) => {
+                db.get(`SELECT * FROM tasks ORDER BY id DESC LIMIT 1`, (err, rows) => {
+                    if (err) reject(err)
+                    else {
+                        resolve(rows)
+                    }
+                })
+            })
+            res.json(lastTask)
         } catch (err) {
             res.status(500).send('Непредвиденная ошибка. Попробуйте позже')
         }
@@ -78,61 +57,44 @@ app.post('/tasks/', (req, res) => {
     }
 })
 
-app.put('/tasks/:id/', (req, res) => {
-    const task = tasks.find(obj => obj.id.toString() === req.params.id)
-    const index = tasks.findIndex(obj => obj.id.toString() === req.params.id)
-
-    if (task) {
-        if (req.body.value) {
-            try {
-                let newTask = {...task}
-                let newTasks = [...tasks]
-                newTask.value = req.body.value
-                newTasks.splice(index, 1, newTask)
-                fs.writeFileSync('taskList.json', JSON.stringify(newTasks))
-                tasks = newTasks
-                async function updateTask() {
-                    return new Promise((resolve, reject) => {
-                        db.run(`UPDATE tasks SET value = ? WHERE id = ?`, [index, newTask.value], (err) => {
-                            if (err) reject(err)
-                            else {
-                                res.json(newTask)
-                                console.log('Успешно')
-                            }
-                        })
-                    })
-                }
-                updateTask().then(data=> data)
-            } catch (err) {
-                res.status(500).send('Непредвиденная ошибка. Попробуйте позже')
-            }
-        } else {
-            res.status(400).send('Поле value обязательно')
+app.put('/tasks/:id/', async (req, res) => {
+    if (req.body.value) {
+        try {
+            const newTaskDB = await new Promise((resolve, reject) => {
+                db.run(`UPDATE tasks SET value = ? WHERE id = ?`, [req.body.value, req.params.id], (err) => {
+                    if (err) reject(err)
+                    else {
+                        resolve(req.body)
+                    }
+                })
+            })
+            res.json(newTaskDB)
+        } catch (err) {
+            res.status(500).send('Непредвиденная ошибка. Попробуйте позже')
         }
     } else {
-        res.status(404).send('Не найдено')
+        res.status(400).send('Поле value обязательно')
     }
 })
 
-app.delete('/tasks/:id/', (req, res) => {
-    const index = tasks.findIndex(obj => obj.id.toString() === req.params.id)
-    let newTasks = [...tasks]
-
-    if (index !== -1) {
+app.delete('/tasks/:id/', async (req, res) => {
+    if (req.params.id !== -1) {
         try {
-            newTasks.splice(index, 1)
-            fs.writeFileSync('taskList.json', JSON.stringify(newTasks))
-            tasks = newTasks
-            res.status(200).send("OK")
-            async function deleteTask() {
-                return new Promise((resolve, reject) => {
-                    db.run(`DELETE FROM tasks WHERE id = ?`, [index], (err) => {
-                        if (err) reject(err)
-                        else {console.log('Успешно')}
-                    })
+            const deleteTask = await new Promise((resolve, reject) => {
+                db.run(`DELETE FROM tasks WHERE id = ?`, [req.params.id], (err) => {
+                    if (err) reject(err)
+                    else {
+                        resolve('OK')
+                    }
                 })
-            }
-            deleteTask().then(data=> data)
+            })
+            const tasks = await new Promise((resolve, reject) => {
+                db.all(`SELECT * FROM tasks`, [], (err, rows) => {
+                    if (err) reject(err)
+                    else resolve(rows)
+                })
+            })
+            res.json(tasks)
         } catch (err) {
             res.status(500).send('Непредвиденная ошибка. Попробуйте позже')
         }
@@ -140,6 +102,8 @@ app.delete('/tasks/:id/', (req, res) => {
         res.status(404).send('Не найдено')
     }
 })
+
+// db.close()
 
 app.listen(port, () => {
     console.log(`has been started on port ${port}...`)
