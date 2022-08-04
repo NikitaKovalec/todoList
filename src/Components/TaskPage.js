d1d68a8e07ba27013d70a575c84d64915e1383d9
nikita <nikitakovalec@gmail.com>
2022-08-04 18:02:34 +0300

    fix code

--- src/Components/TaskPage.js
+++ src/Components/TaskPage.js
@@ -5,34 +5,40 @@ import {setLoadedTasks} from "../redux/action";
 
 function TaskPage() {
 	let [isLoading, setIsLoading] = useState(true)
+	let [isError, setIsError] = useState(false)
 	const params = useParams()
 	const dispatch = useDispatch()
 	let tasks = useSelector(state => state)
 	const id = params.taskId
 
-	const fetchTasks =  async () => {
+	const fetchTasks = async () => {
 		try {
-			const result = fetch(`http://localhost:3100/tasks`, {
+			const result = await fetch('http://localhost:3100/tasks', {
 				mode: 'cors'
 			})
 			if (result.ok) {
-				const data =  await result.json()
+				console.log(result)
+				const data = await result.json()
 				dispatch(setLoadedTasks(data))
+			} else {
+				throw 'err'
 			}
 		} catch (err) {
-			console.log("Ошибка" + err)
+			console.log(err.message)
+			setIsError(true)
 		} finally {
 			setIsLoading(false)
 		}
 	}
+
 	useEffect(() => {
 		fetchTasks()
 	}, [])
 
 	const findTask = tasks.find(obj => obj.id.toString() === id)
-	console.log(tasks)
 
 	return <>
+		{isError ? <div style={{marginLeft: 15}}>Ошибка загрузки данных...</div> : <></>}
 		{isLoading ? <h3>'Загрузка данных... '</h3> : <h3>Текст таска: {findTask.value}</h3>}
 		<Link to='/'>
 			<button>Назад</button>
