3385f6c67c76495ecb50cc62345e98a7cc2997c0
nikita <nikitakovalec@gmail.com>
2022-08-04 17:19:40 +0300

    fix code

--- src/Components/TaskPage.js
+++ src/Components/TaskPage.js
@@ -1,21 +1,28 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {setLoadedTasks} from "../redux/action";
 
 function TaskPage() {
+	let [isLoading, setIsLoading] = useState(true)
 	const params = useParams()
 	const dispatch = useDispatch()
 	let tasks = useSelector(state => state)
 	const id = params.taskId
 
-	const fetchTasks = async () => {
-		const result = fetch(`http://localhost:3100/tasks`, {
-			mode: 'cors'
-		})
-		if (result.ok) {
-			const data = await result.json()
-			dispatch(setLoadedTasks(data))
+	const fetchTasks =  async () => {
+		try {
+			const result = fetch(`http://localhost:3100/tasks`, {
+				mode: 'cors'
+			})
+			if (result.ok) {
+				const data =  await result.json()
+				dispatch(setLoadedTasks(data))
+			}
+		} catch (err) {
+			console.log("Ошибка" + err)
+		} finally {
+			setIsLoading(false)
 		}
 	}
 	useEffect(() => {
@@ -23,9 +30,10 @@ function TaskPage() {
 	}, [])
 
 	const findTask = tasks.find(obj => obj.id.toString() === id)
+	console.log(tasks)
 
 	return <>
-		<h3>Текст таска: {findTask.value}</h3>
+		{isLoading ? <h3>'Загрузка данных... '</h3> : <h3>Текст таска: {findTask.value}</h3>}
 		<Link to='/'>
 			<button>Назад</button>
 		</Link>
